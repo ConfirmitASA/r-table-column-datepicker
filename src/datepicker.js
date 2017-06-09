c69bc8736796f74b27e1c1bd17daac5f87e2db13
annyrtd <fyz95@mail.ru>
2017-06-09 16:24:13 +0300

    set up datepicker logic

--- src/datepicker.js
+++ src/datepicker.js
@@ -2,6 +2,19 @@ var styles = require('./datepicker.css');
 
 class TableColumnDatepicker {
   constructor(options) {
+    var header = options.header;
+    var datepicker = options.datepicker;
+    datepicker.classList.add('hidden');
+    datepicker.classList.add('table-column-datepicker');
+
+    var img = document.createElement('img');
+    img.setAttribute('src', '/cf_clientutil/images/calendar_16.png');
+
+    header.appendChild(img1);
+    img.addEventListener('click', function() {
+      datepicker.classList.toggle('hidden');
+    });
+
   }
 }
 
